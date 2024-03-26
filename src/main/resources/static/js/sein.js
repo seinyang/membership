b2d3592571b1516d464981dbe05cf7265ffdc49e
seinyang <did756984@naver.com>
2024-03-27 01:02:00 +0900

    index 파일 완성 , 업무일지작성 수정중

--- src/main/resources/static/js/sein.js
+++ src/main/resources/static/js/sein.js
@@ -12,3 +12,24 @@ document.getElementById('date').value = getCurrentDate();
 
 
 //스마트 에디터
+let oEditors = []
+
+smartEditor = function() {
+    console.log("Naver SmartEditor")
+    nhn.husky.EZCreator.createInIFrame({
+        oAppRef: oEditors,
+        elPlaceHolder: "editorTxt",
+        sSkinURI: "/css/smarteditor/SmartEditor2Skin.html",
+        fCreator: "createSEditor2"
+    })
+}
+
+$(document).ready(function() {
+    smartEditor()
+    // business textarea의 내용을 에디터에 설정
+    const businessTextarea = document.getElementById('business');
+    if (businessTextarea) {
+        const businessContent = businessTextarea.value;
+        oEditors.getById["editorTxt"].exec("PASTE_HTML", [businessContent]);
+    }
+})
